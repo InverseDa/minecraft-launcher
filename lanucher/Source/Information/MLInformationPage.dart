3157842ed3807f963d282d67edb7ea36989def4b
InverseDa <inverse_boom@outlook.com>
2024-11-20 22:37:38 +0800

    [feature:init] Temp

---
+++ lanucher/Source/Information/MLInformationPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MLInfomationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Information'),
+      ),
+      body: Center(
+        child: Text('Information Page'),
+      ),
+    );
+  }
+}
+
